504a9d1cace0554274fd95c70712c7e962fcd01c
Koyo Kisu <koyokisu@gmail.com>
2021-06-09 16:22:10 +0900

    Updates

---
+++ precache-manifest.261a1dd514527a331b92ddc7603d7edc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc543fd46f0c6c7f4fb5",
+    "url": "/pwa-app/css/app.fb0c6e1c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwa-app/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "79574da30706acb4d70e45ee8ce6e40a",
+    "url": "/pwa-app/index.html"
+  },
+  {
+    "revision": "dc543fd46f0c6c7f4fb5",
+    "url": "/pwa-app/js/app.96d8e757.js"
+  },
+  {
+    "revision": "da287af5732a6105f54f",
+    "url": "/pwa-app/js/chunk-vendors.ec627fa8.js"
+  },
+  {
+    "revision": "0184312ec12c5ed50baa5a94a5e13e04",
+    "url": "/pwa-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa-app/robots.txt"
+  }
+]);
